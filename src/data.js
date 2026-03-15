58f3a1d9aebbd896074e5a97a61fbb555c519cfb
Divyam Joshi <divyamjoshi2004@gmail.com>
2026-03-15 15:45:28 +0530

    first

---
+++ src/data.js
@@ -0,0 +1,60 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const firstHeading = "Happy Birthday Nityam Biro"
+// Main heading text - Change this text as you like
+
+export const firstSubtext = "Since Its Your Birthday I have A little Surprise For You"
+// Subtext under heading (optional) - Change, leave blank or remove.
+
+
+// ==========================
+// 2. Cake Screen
+// ==========================
+// Add recipient's name here.
+export const NAME = "Nityam Biro!"
+
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Some Sweet Moments"
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.jpeg" }, Match the format and add more as needed
+export const photos = [
+    "/images/1.jpeg",
+    "/images/2.jpeg",
+    "/images/3.jpeg",
+    "/images/4.jpeg",
+    "/images/5.jpeg",
+]
+
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "A Special Message"
+// Heading above card - Change this text as you like
+
+export const specialMessage = `Happy Birthday,Nityam Biro! You deserve all the happiness,in the world. You have this special way of making everything around you brighter, your kindness, and the way you make people feel truly cared for. I hope your day is filled with laughter, surprises, and moments that make your heart happy. You’re truly The Bestest Brother I Can Have, and I just want you to know how special you are To Me. Keep being the amazing person you are, spreading joy wherever you go. Wishing you endless happiness, success, and all the sweet things life has to offer. 💗`
+// Main special message (you can edit freely)
+
+
+// ==========================
+// 4. Final Screen
+// ==========================
+export const finalScreenHeading = "One Last Thing..."
+// Heading above final surprise - Change this text as you like
+
+export const overlayText = "Lots of love for you Brother❤️"
+// First text inside the overlay card - Change this text as you like
+
+export const overlayMessage = "Once again, Happy Birthday!And I Hope you loved your surprise. Ab Achi Party Chahiye"
+
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3"
